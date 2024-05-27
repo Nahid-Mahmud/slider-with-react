d3d4be8f312ca42b0427e473e5fa9401c93d1066
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2024-05-27 13:51:30 +0600

    Custom hook for geting image created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { Fragment, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Fragment } from "react";
+import useGetImagesFromApi from "./hooks/useGetImagesFromApi";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const { images } = useGetImagesFromApi({ pageNumber: 1, limit: 5 });
+  console.log(images);
   return (
     <Fragment>
       <div>
---
+++ src/hooks/useGetImagesFromApi.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+export default function useGetImagesFromApi({ pageNumber = 1, limit = 5 }) {
+  const [images, setImages] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const url = `https://picsum.photos/v2/list?page=${pageNumber}&limit=${limit}`;
+
+  useEffect(() => {
+    async function fetchImages() {
+      setLoading(true);
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setImages(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchImages();
+  }, [url]);
+
+  return { images, loading, error };
+}
